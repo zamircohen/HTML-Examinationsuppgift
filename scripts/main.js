3862c7338f1b315cb1e76e0bf096b19ff36a2e13
Zamir Cohen <90824610+zamircohen@users.noreply.github.com>
2021-11-22 10:14:33 +0100

    Add all files

---
+++ scripts/main.js
@@ -0,0 +1,102 @@
+$(function() {
+
+    $(".text1").hide();
+    $(".text2").hide();
+    $(".text3").hide();
+    $(".text4").hide();
+    $(".text5").hide();
+    $(".text6").hide();
+    $(".text7").hide();
+    $(".text8").hide();
+    $(".text9").hide();
+    $(".text10").hide();
+
+    $(".moreInfo1").on('click', function() {
+        $(".text1").toggle("slow")
+        $(this).text(function(i, text) {
+            return text === "Klicka här för att dölja info" ? "Klicka här för mer info" : "Klicka här för att dölja info";
+        })
+    })
+    
+    $(".moreInfo2").on('click', function() {
+        $(".text2").toggle("slow")
+        $(this).text(function(i, text) {
+            return text === "Klicka här för att dölja info" ? "Klicka här för mer info" : "Klicka här för att dölja info";
+        })
+    })
+
+    $(".moreInfo3").on('click', function() {
+        $(".text3").toggle("slow")
+        $(this).text(function(i, text) {
+            return text === "Klicka här för att dölja info" ? "Klicka här för mer info" : "Klicka här för att dölja info";
+        })
+    })
+
+    $(".moreInfo4").on('click', function() {
+        $(".text4").toggle("slow")
+        $(this).text(function(i, text) {
+            return text === "Klicka här för att dölja info" ? "Klicka här för mer info" : "Klicka här för att dölja info";
+        })
+    })
+
+    $(".moreInfo5").on('click', function() {
+        $(".text5").toggle("slow")
+        $(this).text(function(i, text) {
+            return text === "Klicka här för att dölja info" ? "Klicka här för mer info" : "Klicka här för att dölja info";
+        })
+    })
+
+    $(".moreInfo6").on('click', function() {
+        $(".text6").toggle("slow")
+        $(this).text(function(i, text) {
+            return text === "Klicka här för att dölja info" ? "Klicka här för mer info" : "Klicka här för att dölja info";
+        })
+    })
+
+    $(".moreInfo7").on('click', function() {
+        $(".text7").toggle("slow")
+        $(this).text(function(i, text) {
+            return text === "Klicka här för att dölja info" ? "Klicka här för mer info" : "Klicka här för att dölja info";
+        })
+    })
+
+    $(".moreInfo8").on('click', function() {
+        $(".text8").toggle("slow")
+        $(this).text(function(i, text) {
+            return text === "Klicka här för att dölja info" ? "Klicka här för mer info" : "Klicka här för att dölja info";
+        })
+    })
+
+    $(".moreInfo9").on('click', function() {
+        $(".text9").toggle("slow")
+        $(this).text(function(i, text) {
+            return text === "Klicka här för att dölja info" ? "Klicka här för mer info" : "Klicka här för att dölja info";
+        })
+    })
+
+    $(".moreInfo10").on('click', function() {
+        $(".text10").toggle("slow")
+        $(this).text(function(i, text) {
+            return text === "Klicka här för att dölja info" ? "Klicka här för mer info" : "Klicka här för att dölja info";
+        })
+    })
+
+        
+    $("#submitButton").on('click', function(){
+            
+        $('.contact')
+        // .$(this).fadeOut("slow")
+        .text("Tack för ditt meddelande. Vi återkopplar så fort vi kan")
+      });
+
+
+    $("#createButton").on('click', function(){
+            
+        $('.form-style-2')
+        // .$(this).fadeOut("slow")
+        .text("Ditt event är nu skapat")
+      });
+
+})
+
+
